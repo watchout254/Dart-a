4b0a2952542a7dfe9665f41793ea83003c0bfa09
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-02-22 11:57:39 +0300

    Containers

---
+++ variables.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = "Daniel";
+  int age = 22;
+  double height = 1.65;
+  var year = 2024;
+  num student = 460;
+
+  print(name);
+  print(age);
+  print(height);
+  print(year);
+  print(student);
+}
