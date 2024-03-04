1360431530dfe626668e27908d91d43c5b14a268
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-03-04 09:36:33 +0300

    dartaaaa

--- variables.dart
+++ variables.dart
@@ -4,10 +4,12 @@ void main() {
   double height = 1.65;
   var year = 2024;
   num student = 460;
+  bool isCool = True;
 
   print(name);
   print(age);
   print(height);
   print(year);
   print(student);
+  print(isCool);
 }
