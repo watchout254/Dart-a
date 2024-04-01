4e6f4c6955cb64fd4781049a12cb142270295493
Daniel Mukenya <88248852+watchout254@users.noreply.github.com>
2024-04-01 13:35:48 +0300

    list of numbers input & outputs the largest number

---
+++ utility/utility6.dart
@@ -0,0 +1,13 @@
+int findLargest(List<int> numbers) {
+  int largest = numbers[0];
+  for (int i = 1; i < numbers.length; i++) {
+    if (numbers[i] > largest) {
+      largest = numbers[i];
+    }
+  }
+  return largest;
+}
+
+void main() {
+  findLargest([1,2,3,4]);
+}
